bb2d0a7045be72d5a67fb80de5a9d8cbe0a616ad
fabiosiqueira12 <fabio.siqueira1222@gmail.com>
2019-10-04 01:48:20 -0300

    Alterações na home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
+//use window.scrollY
+var scrollpos = window.scrollY;
+var header = document.getElementById("header-principal");
+
+function add_class_on_scroll() {
+    header.classList.add("fixed");
+}
+
+function remove_class_on_scroll() {
+    header.classList.remove("fixed");
+}
+
+window.addEventListener('scroll', function(){ 
+    //Here you forgot to update the value
+    scrollpos = window.scrollY;
+
+    if(scrollpos > 10){
+        add_class_on_scroll();
+    }
+    else {
+        remove_class_on_scroll();
+    }
+});
+
 function toggleMenu() {
     document.getElementsByTagName('html')[0].classList.toggle('menu-active');
 }
